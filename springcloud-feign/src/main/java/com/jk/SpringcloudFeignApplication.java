04cb0be4e176f18529953680267953f2d49ee736
dddhy <wshongyuding@163.com>
2019-09-18 10:08:47 +0800

    dhy 第一次

--- springcloud-feign/src/main/java/com/jk/SpringcloudFeignApplication.java
+++ springcloud-feign/src/main/java/com/jk/SpringcloudFeignApplication.java
@@ -1,4 +1,4 @@
-package com.jk.springcloudfeign;
+package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ springcloud-feign/src/main/java/com/jk/controller/DhyController.java
@@ -0,0 +1,12 @@
+package com.jk.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("")
+@ResponseBody
+public class DhyController {
+
+
+
+}
